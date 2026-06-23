04a93e2bb3f25592492aa2e35afb6cc7a22fb51e
cl-krma <clkourouma688@gmail.com>
2026-06-23 12:45:14 +0000

    mise a jour du footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2026 Cheick L. Dev. Tous droits réservés.</p>
+      <p>© 2026 CL. Tous droits réservés.</p>
       <div className="socials">
         <a href="https://github.com/CL-KRMA" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href="https://www.linkedin.com/in/cheick-laye-kourouma-2b1b423b8/?locale=fr-FR" target="_blank" rel="noopener noreferrer">LinkedIn</a>
